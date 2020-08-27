65cfb3ab72940b5b14ad19c117e2f2821b59449f
John Robling <roblingjohn@gmail.com>
2020-08-27 16:44:05 -0400

    added gender specific nouns to array

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,76 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Display extends Component {
-    state =  {
-        adj: "",
-        noun: "",
-        catData: this.props.location.state.catData
-    }
-    
-    adjWord = ""
+  state = {
+    adj: "",
+    noun: "",
+    catData: this.props.location.state.catData,
+  };
 
-    nounWord = ""
+  adjWord = "";
 
-    componentDidMount() {
-        this.selectName();
-        console.log(this.props.location.state.catData)
-    }
+  nounWord = "";
 
-    adjArray = [
-        "TINY",
-        "BABY",
-        "STINKY",
-        "CUTIE",
-        "LITTLE",
-        "FLUFFY",
-        "SWEET",
-        "SILLY",
-        "SLEEPY",
-        "CUDDLE",
-        "SASSY",
-        "SLINKY",
-        "FUZZY",
-    ]
+  currentNounArray = [];
 
-    nounArray = [
-        "BEAN",
-        "KITTY",
-        "BABE",
-        "BUTT",
-        "BIT",
-        "POTATO",
-        "MUFFIN",
-        "ANGEL",
-        "PIE",
-        "FLOOF",
-        "BALL",
-        "MARSHMALLOW",
-        "MAGICIAN",
-        "CUP",
-        "LOAF",
-    ]
+  componentDidMount() {
+    if (this.props.location.state.catData.sex === "male") {
+      this.currentNounArray = this.nounArray.concat(this.maleNounArray);
+    } else if (this.props.location.state.catData.sex === "female") {
+      this.currentNounArray = this.nounArray.concat(this.femaleNounArray);
+    } else this.currentNounArray = this.nounArray;
+    this.selectName();
+    console.log(this.props.location.state.catData);
+  }
 
-    selectName = ()=> {
-        let adjNumber = Math.floor(Math.random() * this.adjArray.length);
-        let nounNumber = Math.floor(Math.random() * this.nounArray.length)
-        this.adjWord = this.adjArray[adjNumber];
-        this.nounWord = this.nounArray[nounNumber];
-        this.setState({
-            adj: this.adjWord,
-            noun: this.nounWord
-        })
-    }
+  adjArray = [
+    "TINY",
+    "BABY",
+    "STINKY",
+    "CUTIE",
+    "LITTLE",
+    "FLUFFY",
+    "SWEET",
+    "SILLY",
+    "SLEEPY",
+    "CUDDLE",
+    "SASSY",
+    "SLINKY",
+    "FUZZY",
+  ];
 
-    render() {
-        return (
-            <div>
-                <h3>call your cat</h3>
-                <h1>{this.state.adj}</h1>
-                <h1>{this.state.noun}</h1>
-                <button className="newNickname" onClick={this.selectName}>NEW NICKNAME</button>
-            </div>
-        );
-    }
+  nounArray = [
+    "BEAN",
+    "KITTY",
+    "BABE",
+    "BUTT",
+    "BIT",
+    "POTATO",
+    "MUFFIN",
+    "ANGEL",
+    "PIE",
+    "FLOOF",
+    "BALL",
+    "MARSHMALLOW",
+    "CUP",
+    "LOAF",
+  ];
+
+  maleNounArray = ["BOY", "MAN", "GENTLEMAN"];
+
+  femaleNounArray = ["GIRL", "LADY"];
+
+  selectName = () => {
+    let adjNumber = Math.floor(Math.random() * this.adjArray.length);
+    let nounNumber = Math.floor(Math.random() * this.currentNounArray.length);
+    this.adjWord = this.adjArray[adjNumber];
+    this.nounWord = this.currentNounArray[nounNumber];
+    this.setState({
+      adj: this.adjWord,
+      noun: this.nounWord,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>call your cat</h3>
+        <h1>{this.state.adj}</h1>
+        <h1>{this.state.noun}</h1>
+        <button className="btn" onClick={this.selectName}>
+          NEW NICKNAME
+        </button>
+        <br />
+        <Link to="/">
+          <button className="btn">NEW CAT INFO</button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Display;
